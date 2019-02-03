b3d76fff1a7171520159816ddfb06405991c1d20
Vladimir Skripkin <vladimir-scripkin@ya.ru>
2019-02-03 17:15:50 +0300

    add option to compile templates without merging in one file

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ var fs = require('fs'),
 	path = require('path'),
 	through = require('through2'),
 	Vinyl = require('vinyl'),
-	PluginError = require('plugin-error');
+	PluginError = require('plugin-error'),
 
-var ejs = require('./lib/ejs'),
+	ejs = require('./lib/ejs'),
 	sPackageName = require('./package.json').name,
 	sTemplatesTpl = fs.readFileSync(path.join(__dirname,'./lib/templates.js'), {encoding: 'utf8'});
 
@@ -24,14 +24,57 @@ module.exports = function (_options)
 {
 	_options = _options || {};
 
-	var sJoinedContent = '',
-		sModuleName = _options.moduleName || 'templates',
+	var sModuleName = _options.moduleName || 'templates',
 		sTemplates = sTemplatesTpl.replace('/*MODULENAME*/', sModuleName),
-		sContentTpl = 'templates[\'%s\'] = %s;\n\n',
+		sContentTpl = 'templates[\'%s\'] = %s;\n\n';
+
+
+	if (_options.unify === false)
+	{
+		var rEscapeRegExp = /[|\\{}()[\]^$+*?.]/g,
+			sTplType = (_options['tpl-type'] || 'text/template').replace(rEscapeRegExp, '\\$&'),
+			aScript = [
+				'<script\\s+type="' + sTplType +'"\\s+id="([^\'"]+)">([\\s\\S]+?)</script>',
+				'<script\\s+id="([^\'"]+)"\\s+type="' + sTplType +'">([\\s\\S]+?)</script>'
+			],
+			rScript = new RegExp('(?:' + aScript.join('|') + ')', 'g');
+
+		return through.obj(function (_oFile, _sEncoding, _next)
+		{
+			if (_oFile.isNull())
+			{
+				return _next();
+			}
+			if (_oFile.isStream())
+			{
+				return this.emit('error', new PluginError(sPackageName, 'Streaming not supported'));
+			}
+
+
+			var sName = path.relative(_oFile.base, _oFile.path),
+				sContent = _oFile.contents.toString('utf8');
+
+			sContent = sContent.replace(rScript, function (_sMatch, _ID, _sTpl)
+			{
+				return util.format(sContentTpl, _ID, ejs.compile(_sTpl, _options));
+			})
+			.trim().replace(/^/gm, '\t');
+
+			_oFile.contents = new Buffer(sTemplates.replace('/*PLACEHOLDER*/', sContent));
+			_oFile.path = path.join(__dirname, _oFile.stem + '.js');
+
+			this.push(_oFile);
+			_next();
+		});
+	}
+
+
+	var sFileName = _options.fileName || sModuleName,
+		sJoinedContent = '',
 		oJoinedFile = new Vinyl({
 			cwd: __dirname,
 			base: __dirname,
-			path: path.join(__dirname, sModuleName + '.js')
+			path: path.join(__dirname, sFileName + '.js')
 		});
 
 	return through.obj(function (_oFile, _sEncoding, _next)
@@ -60,6 +103,7 @@ module.exports = function (_options)
 		this.push(null);
 	});
 
+
 	function template_name (_sTpl, _sName)
 	{
 		var aMatch = /^\/\* ([a-zA-Z0-9_-]+) \*\/ /.exec(_sTpl);
@@ -70,5 +114,5 @@ module.exports = function (_options)
 		}
 
 		return _sName.replace(/\\/g, '/').replace(path.extname(_sName), '');
-	}
+	};
 };
